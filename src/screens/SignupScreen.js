6bcbc41ea96680f1fc012867896a7be4ce415b49
Gurcan Hamali <gurcanhamali@gmail.com>
2020-11-15 19:07:17 +1100

    216. Last Bit of Extracting

--- src/screens/SignupScreen.js
+++ src/screens/SignupScreen.js
@@ -1,6 +1,6 @@
-import React, { useState, useContext } from 'react';
+import React, { useContext } from 'react';
 import { View, StyleSheet, TouchableOpacity } from 'react-native';
-import { Text, Input, Button } from 'react-native-elements';
+import { Text } from 'react-native-elements';
 
 import Spacer from '../components/Spacer';
 import { Context as AuthContext } from '../context/AuthContext';
@@ -40,12 +40,7 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     marginBottom: 25
   },
-  errorMessage: {
-    fontSize: 16,
-    color: 'red',
-    marginLeft: 15,
-    marginTop: 15
-  },
+
   link: {
     color: 'blue'
   }
