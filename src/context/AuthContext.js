18b975a2ecb44f9439e9ab7b63c81ff1edb3dcd7
Gurcan Hamali <gurcanhamali@gmail.com>
2020-11-15 15:34:02 +1100

    209. Handling Errored Requests

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -3,19 +3,23 @@ import trackerApi from '../api/tracker';
 
 const authReducer = (state, action) => {
   switch (action.type) {
+    case 'add_error':
+      return { ...state, errorMessage: action.payload };
     default:
       return state;
   }
 };
 
 const signup = dispatch => {
-  console.log('ddd');
   return async ({ email, password }) => {
     try {
       const response = await trackerApi.post('/signup', { email, password });
       console.log(response.data);
     } catch (err) {
-      console.log(err.response.data);
+      dispatch({
+        type: 'add_error',
+        payload: 'Something went wrong with sign up'
+      });
     }
   };
 };
@@ -37,5 +41,5 @@ const signout = dispatch => {
 export const { Provider, Context } = createDataContext(
   authReducer,
   { signin, signout, signup },
-  { isSignedIn: false }
+  { isSignedIn: false, errorMessage: '' }
 );
