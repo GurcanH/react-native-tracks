6f7bad1532159797604276a823840ebf669c1944
Gurcan Hamali <gurcanhamali@gmail.com>
2020-11-15 15:23:27 +1100

    208. Making an API Request

--- src/screens/SignupScreen.js
+++ src/screens/SignupScreen.js
@@ -1,11 +1,13 @@
-import React, { useState } from 'react';
+import React, { useState, useContext } from 'react';
 import { View, StyleSheet } from 'react-native';
 import { Text, Input, Button } from 'react-native-elements';
-import { set } from 'react-native-reanimated';
 
 import Spacer from '../components/Spacer';
+import { Context as AuthContext } from '../context/AuthContext';
 
 const SignupScreen = ({ navigation }) => {
+  const { state, signup } = useContext(AuthContext);
+
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
@@ -32,7 +34,7 @@ const SignupScreen = ({ navigation }) => {
       />
       <Spacer />
       <Spacer>
-        <Button title='Sign Up' />
+        <Button title='Sign Up' onPress={() => signup({ email, password })} />
       </Spacer>
     </View>
   );
