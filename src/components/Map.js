05d20786a968b610157a88a2ce52147a4b29e5da
Gurcan Hamali <gurcanhamali@gmail.com>
2020-11-16 12:59:23 +1100

    241. Live Location Updates

--- src/components/Map.js
+++ src/components/Map.js
@@ -20,11 +20,11 @@ const Map = () => {
         latitudeDelta: 0.01,
         longitudeDelta: 0.01
       }}
-      region={{
-        ...currentLocation.coords,
-        latitudeDelta: 0.01,
-        longitudeDelta: 0.01
-      }}
+      //   region={{
+      //     ...currentLocation.coords,
+      //     latitudeDelta: 0.01,
+      //     longitudeDelta: 0.01
+      //   }}
     ></MapView>
   );
 };
