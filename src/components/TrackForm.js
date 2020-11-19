af5d4cd26a846dbe55b38badb1571ddf3fc4f0fe
Gurcan Hamali <gurcanhamali@gmail.com>
2020-11-19 22:32:00 +1100

    258. What Manages Tracks

--- src/components/TrackForm.js
+++ src/components/TrackForm.js
@@ -16,11 +16,18 @@ const TrackForm = () => {
     <>
       <Input value={name} onChangeText={changeName} placeholder='Enter name' />
       <Spacer />
-      {recording ? (
-        <Button title='Stop' onPress={stopRecording} />
-      ) : (
-        <Button title='Start Recording' onPress={startRecording} />
-      )}
+      <Spacer>
+        {recording ? (
+          <Button title='Stop' onPress={stopRecording} />
+        ) : (
+          <Button title='Start Recording' onPress={startRecording} />
+        )}
+      </Spacer>
+      <Spacer>
+        {!recording && locations.length ? (
+          <Button title='Save Recording' />
+        ) : null}
+      </Spacer>
     </>
   );
 };
